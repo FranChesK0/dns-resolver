bc933da23075da7e83dc902ad5603130b8793ebe
FranChesK0 <dev.franchesko@gmail.com>
2024-10-10 11:01:07 +0800

    add parse header function

--- internal/query/header.go
+++ internal/query/header.go
@@ -3,6 +3,7 @@ package query
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 )
 
 const RECURSION_FLAG uint16 = 1 << 8
@@ -36,4 +37,23 @@ func (h *Header) ToBytes() []byte {
 	binary.Write(encodedHeader, binary.BigEndian, h.NsCount)
 	binary.Write(encodedHeader, binary.BigEndian, h.ArCount)
 	return encodedHeader.Bytes()
+}
+
+func ParseHeader(reader *bytes.Reader) (*Header, error) {
+	var header Header
+	binary.Read(reader, binary.BigEndian, &header.Id)
+	binary.Read(reader, binary.BigEndian, &header.Flags)
+	switch header.Flags & 0b1111 {
+	case 1:
+		return nil, errors.New("query error")
+	case 2:
+		return nil, errors.New("server error")
+	case 3:
+		return nil, errors.New("the domain does not exist")
+	}
+	binary.Read(reader, binary.BigEndian, &header.QdCount)
+	binary.Read(reader, binary.BigEndian, &header.AnCount)
+	binary.Read(reader, binary.BigEndian, &header.NsCount)
+	binary.Read(reader, binary.BigEndian, &header.ArCount)
+	return &header, nil
 }
